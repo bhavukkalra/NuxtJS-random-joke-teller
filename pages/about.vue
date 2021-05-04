5965f10f6b7ce390ab1684cfefdce336e4183f9c
bhavukkalra <bhavukkalra1786@gmail.com>
2021-05-04 19:02:31 +0530

    Prettier used to indent code

--- pages/about.vue
+++ pages/about.vue
@@ -1,40 +1,26 @@
-`<template>
-
-
-<div>
-
-
+`
+<template>
+  <div>
     <h1>About Dad jokes</h1>
     <p>App that displays corny dad jokes</p>
-
-
-</div>
-
-
-    
+  </div>
 </template>
-
 
 <script>
 export default {
-
-    head(){
-        return{
-            title: "About the app",
-            meta: [
-                {
-                    hid: "description",
-                    name: "description",
-                    content: 'best place for jokes'
-                }
-            ]
-        }
+  head() {
+    return {
+      title: 'About the app',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'best place for jokes',
+        },
+      ],
     }
-    
+  },
 }
 </script>
 
-
-<style>
-
-</style>
+<style></style>
