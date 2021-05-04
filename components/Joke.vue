ea18798cd9e9773472a726f6ee15b93388b736c8
bhavukkalra <bhavukkalra1786@gmail.com>
2021-05-04 19:06:31 +0530

    Made the Jokes gotten parameterized and clickable

--- components/Joke.vue
+++ components/Joke.vue
@@ -1,9 +1,18 @@
 <!--This component will be rendered for each joke that is received  -->
 <template>
-  <div class="joke">
+  <!-- :to used for dynamic linking with export default(to for simple) -->
+
+  <Nuxt-link :to="'jokes/' + id">
+    <div class="joke">
+      <h2>Sample Text here for testing something</h2>
+      <p>{{ joke }}</p>
+    </div>
+  </Nuxt-link>
+
+  <!-- <div class="joke">
     <h2>Sample Text here for testing something</h2>
     <p>{{ joke }}</p>
-  </div>
+  </div> -->
 </template>
 
 <script>
