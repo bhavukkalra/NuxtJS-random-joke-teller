a7e1d40c9d6817d53f2581b3d9770104cdd28553
bhavukkalra <bhavukkalra1786@gmail.com>
2021-05-04 19:07:06 +0530

    searching functionality added

---
+++ components/SearchJokes.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="text" placeholder="Search jokes" />
+    <input type="submit" value="Search Jokes" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchJokes',
+
+  data() {
+    return {
+      text: '',
+    }
+  },
+
+  methods: {
+    //  On submit, send the text entered to to parent Component
+    //  with 'search-text' placeholder and this.text as a parameter
+    onSubmit() {
+      console.log('Inside on submit functions')
+      this.$emit('search-text', this.text)
+      this.text = ''
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Jokes domain</h1>
+    <!-- We are emiting an event called search-text from children component-->
+    <!-- When it is received Invoke a Method searchText -->
+    <SearchJokes v-on:search-text="searchText" />
+
     <!-- <Joke v-for="joke in jokes" v-bind:key=joke.id /> -->
     <Joke
       v-for="joke in jokes"
@@ -14,10 +18,12 @@
 <script>
 import axios from 'axios'
 import Joke from '../../components/Joke'
+import SearchJokes from '../../components/SearchJokes'
 
 export default {
   components: {
     Joke,
+    SearchJokes,
   },
 
   data() {
@@ -38,10 +44,33 @@ export default {
 
       this.jokes = res.data.results
 
-      console.log('The response that was received is', this.jokes)
+      //   console.log('The response that was received is', this.jokes)
     } catch (error) {
       console.log(error)
     }
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: 'application/json',
+        },
+      }
+
+      try {
+        const res = await axios.get(
+          `https://icanhazdadjoke.com/search?term=${text}`,
+          config
+        )
+        this.jokes = res.data.results
+        // console.log('The text received is', text)
+        // console.log('The data received is', res.data.results)
+
+        // console.log('The response that was received is', this.jokes)
+      } catch (error) {
+        console.log(error)
+      }
+    },
   },
 
   head() {
